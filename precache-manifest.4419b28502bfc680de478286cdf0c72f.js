447570c9cce3d9959368d89dd363d0601bf94067
Adasinho <adirm10@yahoo.com>
2020-03-05 16:05:48 +0100

    Updates

---
+++ precache-manifest.4419b28502bfc680de478286cdf0c72f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2ea8f1875dbc0788afb74923dd4df6",
+    "url": "/rainbowsixsiegestats/index.html"
+  },
+  {
+    "revision": "0a5f59783eae2f58bf89",
+    "url": "/rainbowsixsiegestats/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "80d7db579de067be1264",
+    "url": "/rainbowsixsiegestats/static/css/main.7ec3891c.chunk.css"
+  },
+  {
+    "revision": "0a5f59783eae2f58bf89",
+    "url": "/rainbowsixsiegestats/static/js/2.58a39da8.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/rainbowsixsiegestats/static/js/2.58a39da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80d7db579de067be1264",
+    "url": "/rainbowsixsiegestats/static/js/main.89dfb1c3.chunk.js"
+  },
+  {
+    "revision": "4cb7aca8a307d59f6be4",
+    "url": "/rainbowsixsiegestats/static/js/runtime-main.9c4eaa65.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rainbowsixsiegestats/static/media/logo.5d5d9eef.svg"
+  }
+]);
